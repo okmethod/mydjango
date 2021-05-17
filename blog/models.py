e58119e40297024f9485a4e15bb69edd19fac649
okmethod <83124702+okmethod@users.noreply.github.com>
2021-05-17 15:28:51 +0900

    add comment form and some functions

--- blog/models.py
+++ blog/models.py
@@ -14,5 +14,31 @@ class Post(models.Model):
         self.published_date = timezone.now()
         self.save()
 
+    def unpublish(self):
+        self.published_date = None
+        self.save()
+
+    def allowed_comments(self):
+        return self.comments.filter(is_excluded=False)
+
     def __str__(self):
         return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    is_excluded = models.BooleanField(default=False)
+
+    def allow(self):
+        self.is_excluded = False
+        self.save()
+
+    def exclude(self):
+        self.is_excluded = True
+        self.save()
+
+    def __str__(self):
+        return self.text
--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,16 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
     path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+    path('post/<int:pk>/publish/', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/unpublish/', views.post_unpublish, name='post_unpublish'),
+    path('post/<int:pk>/remove/', views.post_remove, name='post_remove'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('post/<int:pk>/comment/allow/', views.comment_allow, name='comment_allow'),
+    path('post/<int:pk>/comment/exclude/', views.comment_exclude, name='comment_exclude'),
+    path('post/<int:pk>/comment/remove/', views.comment_remove, name='comment_remove'),
+
 ]
